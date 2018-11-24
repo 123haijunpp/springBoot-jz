d3ccfc4f62ce8fcf170c19990d85467c8bcd86ab
FightingMrQiu <3524422399@qq.com>
2018-11-24 23:11:26 +0800

    这是2018/11/24周六晚上提交的一个测试文件

--- src/main/java/com/qiuzq/jz/Temp/Test.java
+++ src/main/java/com/qiuzq/jz/Temp/Test.java
@@ -1,5 +1,8 @@
 package com.qiuzq.jz.Temp;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 /**
  * program: jz
  * Date: 2018/11/22/022 20:11
@@ -14,5 +17,17 @@ public class Test {
         System.out.println("========上面为继承方法，下面为jdk1.8中新特性，非抽象方法======");
         animal.eat("猪");
         animal.run("狗");
+    }
+
+    @org.junit.Test
+    public void currentHashMap() throws InterruptedException {
+        final HashMap<String, String> map = new HashMap<>(2);
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                new Thread(() -> map.put(UUID.randomUUID().toString(), ""), "ftf" + i).start();
+            }
+        }, "ftf");
+        t.start();
+        t.join();
     }
 }
